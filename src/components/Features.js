ca105e75fc1c9740b37194763e09a923b86300ed
eren kazan <erennkazan@gmail.com>
2023-05-21 22:11:00 +0300

    features added

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react"
+import Features from '../api/features.json'
+import FeaturesItems from './ui/FeaturesItem'
+
+
 export default function Features(){
+
+  const[features, setFeatures] = useState([]);
+
+  useEffect(()=>{
+    setFeatures(Package);
+  },[])
+
     return(
         <div className="bg-brand-color ">
-          <h1>Features</h1>
+          <div>
+            {features.length && features.map((features,index)=> <FeaturesItem key={index} package={features}/>)}
+          </div>
         </div>
     )
 }
