43ffad9c6d3b24eb5aa7c685c27f157e628b226f
eren kazan <erennkazan@gmail.com>
2023-05-29 02:20:40 +0300

    innerpages slider added

--- src/components/InnerPages.js
+++ src/components/InnerPages.js
@@ -1,7 +1,50 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
 export default function InnerPages(){
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
+
     return(
-        <div className="bg-brand-color ">
-          <h1>InnerPages</h1>
+        <div className="container">
+          <div className="mt-5">
+            <p>
+              Ready To Use
+            </p>
+            <h2>Elegant And Impressive Inner Pages</h2>
+          </div>
+          <div>
+        <h2>Auto Play</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
         </div>
     )
 }
