301ebd95f6a9b04925e01ecd16d082aae5194523
eren kazan <erennkazan@gmail.com>
2023-05-29 02:30:45 +0300

    inner pages fixed

--- src/components/InnerPages.js
+++ src/components/InnerPages.js
@@ -4,44 +4,43 @@ import Slider from "react-slick";
 export default function InnerPages(){
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
+    speed: 4000,
+    autoplaySpeed: 1000,
     cssEase: "linear"
   };
 
     return(
-        <div className="container">
-          <div className="mt-5">
-            <p>
+        <div>
+          <div className="container mt-5">
+            <p className="text-inner-color fs-4">
               Ready To Use
             </p>
-            <h2>Elegant And Impressive Inner Pages</h2>
+            <h2 className="text-demo-font-size w-50">Elegant And Impressive Inner Pages</h2>
           </div>
-          <div>
-        <h2>Auto Play</h2>
+          <div className="mt-5">
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+          <div className="w-50">
+            <img src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/demo-slide-4.png" />
           </div>
           <div>
-            <h3>2</h3>
+          <img src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/demo-slide-4.png" />
           </div>
           <div>
-            <h3>3</h3>
+          <img src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/demo-slide-4.png" />
           </div>
           <div>
-            <h3>4</h3>
+          <img src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/demo-slide-4.png" />
           </div>
           <div>
-            <h3>5</h3>
+          <img src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/demo-slide-4.png" />
           </div>
           <div>
-            <h3>6</h3>
+          <img src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/demo-slide-4.png" />
           </div>
         </Slider>
       </div>
