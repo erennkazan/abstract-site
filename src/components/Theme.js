5188b724d10aae460530b2c78d17356850ade6fe
eren kazan <erennkazan@gmail.com>
2023-05-29 22:47:08 +0300

    theme component header text added

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -2,8 +2,10 @@ export default function Theme(){
     return(
         <div className="theme-bg-color">
           <div className="container">
-          <img className="d-block" src="https://new.axilthemes.com/themes/splash/abstrak/assets/media/others/line-6.png"/>
-            <h3>Why Buy Theme From Us?</h3>
+            <div className="d-flex justify-content-between align-items-center w-100 py-5">
+            <h3 className="text-hero-font-size w-50">Why Buy Theme From Us?</h3>
+            <p className="w-25 theme-text-size">Every Theme is built with such efforts that they are ready to meet all of our client's expectations.</p>
+            </div>
           </div>
         </div>
     )
